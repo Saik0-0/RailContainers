d582e2eb1f92a281668fdd382fe2fa2d383c4cba
Litvinenko Alexandra <alexandralit09022005@gmail.com>
2024-11-02 11:27:10 +0300

    1

---
+++ 1.py
@@ -0,0 +1,97 @@
+import pyodbc
+
+SERVER = 'MSI\\SQLEXPRESS'
+DATABASE = 'TZ'
+
+connection_string = (f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};Trusted_Connection'
+                     f'=yes;')
+connection = pyodbc.connect(connection_string)
+cursor = connection.cursor()
+
+"""
+Множество всех контейнеров
+Значения по столбцам:   
+SessionID   
+contNumber   
+Size    
+CargoWeight    
+TareWeight  
+Status  
+Priority    
+Batch   
+Stack   
+DangerClass
+"""
+containers = "SELECT * FROM dbo.upContainersVX"
+cursor.execute(containers)
+containers_array = cursor.fetchall()
+
+"""
+Множество всех платформ
+Значения по столбцам:   
+SessionID   вроде как номер отправки
+carNumber   
+Model   модель
+Size    длина
+CarryingCapacity    грузоподъёмность
+Priority    приоритет
+"""
+platforms = 'SELECT * FROM dbo.upPlatformsVX'
+cursor.execute(platforms)
+platforms_array = cursor.fetchall()
+
+"""Варианты размещения контейнеров на моделях платформы
+Значения по столбцам:
+Код/вариант размещения(один на несколько строчек, сгруппировать по ним)
+Модель платформы(также сгруппировать по ним)
+Классы опасности, которые могут быть на этой платформе
+Номера контейнеров
+Длина контейнера
+Индекс на платформе куда ставим контейнер
+Статус контейнера(куда отправляется)
+ID
+
+Связь с отчётом:
+Каждая модель платформы  характеризуется множеством вариантов  размещения контейнеров на ней
+Хар-ки размещения:
+количество контейнеров, на которое рассчитана модель платформы
+требуемую длину контейнера для постановки его на i-ую позицию
+требуемое состояние контейнера для постановки его на i-ую позицию
+множество классов опасности грузов
+"""
+containers_rules = 'SELECT * FROM dbo.upContainersSPR'
+cursor.execute(containers_rules)
+containers_rules_array = cursor.fetchall()
+
+"""Требования к контейнерам при выбранном вар-те размещения
+Значения по столбцам:
+Код/вариант размещения
+Модель платформы
+Какой критерий рассматриваем
+Номера контейнера на платформе(Первый, второй...)
+Какое ограничение(<=/>=)
+Значение ограничения
+ID
+"""
+containers_on_platforms_rules = 'SELECT * FROM dbo.upRulesSPR'
+cursor.execute(containers_on_platforms_rules)
+containers_on_platforms_rules_array = cursor.fetchall()
+
+"""Набор правил 'если..., то...' для каждой модели
+Нужно транспонировать и сгруппировать по коду размещения
+Значения по столбцам:
+Код/вариант размещения
+Модель платформы
+Какой критерий рассматриваем
+Номер контейнера на платформе(Первый, второй...)
+Нижняя граница значения
+Верхняя граница значения
+ID
+"""
+containers_on_platforms_tables = 'SELECT * FROM dbo.upTablesSPR'
+cursor.execute(containers_on_platforms_tables)
+containers_on_platforms_tables_array = cursor.fetchall()
+
+
+cursor.close()
+connection.close()
