9551fcb68e5ffbbe36eee08c32e2457546997ad9
Litvinenko Alexandra <alexandralit09022005@gmail.com>
2024-11-11 15:36:20 +0300

    make containersSPR

--- 1.py
+++ 1.py
@@ -1,5 +1,7 @@
 import pyodbc
 import numpy as np
+from collections import defaultdict
+
 
 SERVER = 'MSI\\SQLEXPRESS'
 DATABASE = 'TZ'
@@ -84,7 +86,7 @@ containers_on_platforms_rules_array = cursor.fetchall()
 Код/вариант размещения
 Модель платформы
 Какой критерий рассматриваем
-Номер контейнера на платформе(Первый, второй...)
+Номер контейнера на платформе(Первый, второй...) 
 Нижняя граница значения
 Верхняя граница значения
 ID
@@ -92,6 +94,39 @@ ID
 containers_on_platforms_tables = 'SELECT * FROM dbo.upTablesSPR'
 cursor.execute(containers_on_platforms_tables)
 containers_on_platforms_tables_array = cursor.fetchall()
+
+
+"""Функция для сортировки таблицы-правила по моделям и по количеству платформ на ней"""
+
+
+def sort_SPR(table):
+    result = defaultdict(lambda: defaultdict(list))
+
+    data_array = np.array(table, dtype=object)
+
+    for sublist in data_array:
+        for item in sublist:
+            second_key = item[1]  # Вводим второй элемент - модель - как ключ
+            first_elem = item[0]  # Первый элемент - код - для группировки
+
+            result[second_key][first_elem].append(item)  # Добавляем подсписки в структуру
+
+    final_result = defaultdict(dict)
+
+    for second_key, groups in result.items():
+        for first_elem, items in groups.items():
+            count_key = len(items)  # количество подсписков для данного кода
+            if count_key not in final_result[second_key]:
+                final_result[second_key][count_key] = []  # создаём новый список для этого количества дубликатов
+            final_result[second_key][count_key].extend(items)  # добавляем строки
+
+    # Преобразуем в обычный словарь для дальнейшей работы
+    final_result = {k: dict(v) for k, v in final_result.items()}
+    for key in final_result:
+        final_result[key] = {k: final_result[key][k] for k in sorted(final_result[key])}
+
+    return final_result
+
 
 #   Создаём сокращенное множество типов платформ P(т.е. уникальные строки без учёта SessionID и carNumber)
 P = np.delete(np.array(platforms_array), (0, 1), 1)
@@ -118,8 +153,39 @@ for b in B:
     C_b_temp = C[C[:, 6] == b]
     C_b.append(C_b_temp.tolist())
 
+#   Создаём множество классов опасности грузов
 D = set(map(str, np.concatenate([np.array(item.split(',')) if ',' in item else np.array([item])
-                                 for item in np.unique(C[:, -1])])))   # мн-во классов опасности грузов
+                                 for item in np.unique(C[:, -1])])))
+
+
+"""Работаем с таблицей ContainersSPR"""
+platforms_rules_1 = np.unique(np.array(containers_rules_array)[:, 1])
+cont_rules_1 = []
+for platform in platforms_rules_1:
+    cont_rules_1.append((np.array(containers_rules_array)[np.array(containers_rules_array)[:, 1] == platform]).tolist())
+
+containersSPR = sort_SPR(cont_rules_1)
+print(containersSPR)
+
+
+# """Работаем с таблицей RulesSPR"""
+# rules = np.array(containers_on_platforms_rules_array)
+# codes = np.unique(rules[:, 0])
+# rules_2 = []
+# for code in codes:
+#     rules_2.append(rules[rules[:, 0] == code].tolist())
+#
+# rulesSPR = sort_SPR(rules_2)
+#
+# """Работаем с таблицей TablesSPR"""
+# tables = np.array(containers_on_platforms_tables_array)
+# codes = np.unique(tables[:, 0])
+# tables_new = []
+# for code in codes:
+#     tables_new.append(tables[tables[:, 0] == code].tolist())
+#
+# tablesSPR = sort_SPR(tables_new)
+
 
 cursor.close()
 connection.close()
