a1409985a59af50805504c85781996651cd2a8e7
Litvinenko Alexandra <alexandralit09022005@gmail.com>
2024-11-16 12:00:29 +0300

    make func for P, duplP, SC(combinations length 2 and 3) for each platform, work with containersSPR

--- 1.py
+++ 1.py
@@ -1,6 +1,11 @@
+import json
+
+import yaml
+
 import pyodbc
 import numpy as np
 from collections import defaultdict
+import itertools
 
 
 SERVER = 'MSI\\SQLEXPRESS'
@@ -101,14 +106,11 @@ containers_on_platforms_tables_array = cursor.fetchall()
 
 def sort_SPR(table):
     result = defaultdict(lambda: defaultdict(list))
-
     data_array = np.array(table, dtype=object)
-
     for sublist in data_array:
         for item in sublist:
             second_key = item[1]  # Вводим второй элемент - модель - как ключ
             first_elem = item[0]  # Первый элемент - код - для группировки
-
             result[second_key][first_elem].append(item)  # Добавляем подсписки в структуру
 
     final_result = defaultdict(dict)
@@ -128,15 +130,39 @@ def sort_SPR(table):
     return final_result
 
 
-"""Функция разделения входных таблиц контейнеров и платформ на сессии
+def generate_combinations_and_permutations_of_cont(cont_table):
+    cont_combinations = []
+    # for i in range(2, 3):
+    for i in range(2, 4):
+        for comb in itertools.combinations(cont_table, i):
+            if len(set(np.array(comb)[:, 7])) == 1 and len(set(np.array(comb)[:, 5])) == 1:
+                cont_combinations.extend(itertools.permutations(comb))
+
+    cont_combinations[:0] = cont_table
+    return cont_combinations
+
+
+# test = [[201, 'CLHU3959860', 20, 21756, 2170, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'HNKU1009723', 20, 21836, 2140, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'HNKU1009739', 20, 21756, 2140, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'HPCU2193814', 20, 21736, 2120, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'HPCU2193835', 20, 21756, 2120, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'XFVU2930684', 20, 21756, 2185, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'XFVU2990087', 20, 21716, 2185, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'XHCU2365092', 20, 21776, 2110, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'XHCU2365106', 20, 21736, 2110, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'BEAU2450597', 20, 21700, 2210, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'TCKU2054460', 20, 21700, 2230, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'TCKU2720628', 20, 21700, 2230, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'HPSU6356362', 40, 10000, 3860, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1975076', 20, 20000, 2180, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'GESU5255760', 40, 10000, 4150, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'RHNU6001497', 40, 10000, 4200, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKHU9416517', 40, 25000, 3840, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1507645', 20, 20970, 2240, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1999442', 20, 20000, 2180, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'TLVU6002136', 40, 10000, 4180, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'CXDU1607197', 20, 22000, 2250, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'DRYU1960164', 20, 22000, 2260, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'HALU2049926', 20, 21500, 2220, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKHU8721239', 40, 26000, 3890, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SEKU4426512', 40, 26000, 3700, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'GESU3332425', 20, 21700, 2200, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'BEAU2990893', 20, 20640, 2210, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'FCIU6200069', 20, 20640, 2100, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'HALU2084423', 20, 20940, 2230, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'HALU2109394', 20, 20640, 2100, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SEGU1477234', 20, 20640, 2180, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1332629', 20, 20640, 2240, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1437506', 20, 20900, 2240, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1628760', 20, 20640, 2240, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1923909', 20, 20640, 2180, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'TCKU3477087', 20, 20640, 2230, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'TEMU0029020', 20, 20640, 2180, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'TEMU4039773', 20, 20640, 2200, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1344255', 20, 21480, 2300, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1514557', 20, 21220, 2240, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1550955', 20, 21060, 2240, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'RHNU6000187', 40, 10000, 4200, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'RHNU4005240', 20, 9000, 2580, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1204269', 20, 21700, 2300, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'PCIU2674368', 20, 21700, 2320, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1417264', 20, 5100, 2300, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'RHNU6001898', 40, 10000, 4200, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'RHNU6000464', 40, 10000, 4200, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'DRYU2825404', 20, 18500, 2200, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1306065', 20, 23700, 2240, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'SKLU1584662', 20, 600, 2240, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'CLHU3526562', 20, 21700, 2220, 'ВНТР', 0, 22, '5-10-B-1', ''], [201, 'SKLU0712964', 20, 21700, 2300, 'ВНТР', 0, 22, '5-6-D-1', ''], [201, 'TCLU6776366', 20, 21700, 2300, 'ВНТР', 0, 22, '5-10-B-2', ''], [201, 'HALU2072016', 20, 21700, 2220, 'ВНТР', 0, 22, '5-6-D-3', ''], [201, 'SKLU1950340', 20, 21700, 2180, 'ВНТР', 0, 22, '5-6-A-2', ''], [201, 'XHCU2358154', 20, 21700, 2110, 'ВНТР', 0, 22, '5-10-A-1', ''], [201, 'WBPU1961584', 20, 22015, 2230, 'ВНТР', 0, 22, '5-6-A-1', ''], [201, 'HALU5681000', 40, 26000, 3890, 'ВНТР', 0, 22, '25-9-C-1', ''], [201, 'HALU5682049', 40, 26000, 3820, 'ВНТР', 0, 22, '25-9-D-1', ''], [201, 'SKHU9945151', 40, 26000, 3700, 'ВНТР', 0, 22, '25-9-C-2', ''], [201, 'HALU2084629', 20, 21700, 2230, 'ВНТР', 0, 22, '5-6-B-2', ''], [201, 'SEGU1814340', 20, 21700, 2180, 'ВНТР', 0, 22, '5-10-A-2', ''], [201, 'SKLU0722026', 20, 21700, 2300, 'ВНТР', 0, 22, '5-6-A-4', ''], [201, 'SKLU1620250', 20, 21700, 2240, 'ВНТР', 0, 22, '5-10-A-3', ''], [201, 'SKLU2061692', 20, 21700, 2180, 'ВНТР', 0, 22, '5-6-C-4', ''], [201, 'SKLU1911087', 20, 21700, 2180, 'ВНТР', 0, 22, '5-10-B-3', ''], [201, 'SKLU2080481', 20, 21700, 2180, 'ВНТР', 0, 22, '5-10-C-1', ''], [201, 'TEMU1226364', 20, 21700, 2180, 'ВНТР', 0, 22, '5-6-D-2', ''], [201, 'SKLU1656063', 20, 21700, 2240, 'ВНТР', 0, 22, '5-6-A-3', ''], [201, 'BMOU4536354', 40, 22300, 3860, 'ВНТР', 0, 22, '25-9-C-3', ''], [201, 'SWFU2003607', 20, 21700, 2200, 'ВНТР', 0, 22, '5-6-C-1', ''], [201, 'BEAU2346026', 20, 21700, 2210, 'ВНТР', 0, 22, '5-6-B-3', ''], [201, 'HALU2042460', 20, 21700, 2220, 'ВНТР', 0, 22, '5-6-C-2', ''], [201, 'SKLU3515613', 20, 21700, 2240, 'ВНТР', 0, 22, '5-6-B-4', ''], [201, 'SKLU2056952', 20, 21700, 2180, 'ВНТР', 0, 22, '5-6-B-1', ''], [201, 'HALU2086529', 20, 23733, 2220, 'ВНТР', 0, 22, '4-2-C-1', ''], [201, 'SKLU0706149', 20, 23776, 2300, 'ВНТР', 0, 22, '4-9-D-2', ''], [201, 'SKLU1420313', 20, 23905, 2300, 'ВНТР', 0, 22, '4-9-C-1', ''], [201, 'SKLU2029920', 20, 23862, 2180, 'ВНТР', 0, 22, '4-9-C-2', ''], [201, 'UETU2232223', 20, 21700, 2180, 'ВНТР', 0, 22, '4-6-C-2', ''], [201, 'JLTU1160713', 20, 21700, 2160, 'ВНТР', 0, 22, '5-6-C-3', ''], [201, 'OOLU1181261', 20, 21700, 2260, 'ВНТР', 0, 22, '4-6-C-1', ''], [201, 'SKHU9934877', 40, 25998, 3700, 'ВНТР', 0, 22, '12-7-B-1', ''], [201, 'SKLU1631255', 20, 21500, 2240, 'ВНТР', 0, 22, '4-6-C-3', '']]
+# print(generate_combinations_and_permutations_of_cont(test))
+# test = {201: [[[[[201, 'dfvdv'], [201, 'dfvsd']], {(201, 'dfvdv'): 2, (201, 'dfvsd'): 4}]], [[201, 'CLHU3959860', 20, 21756, 2170, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'HNKU1009723', 20, 21836, 2140, 'ВНТР', 0, 0, 'O-T-P-R', ''], [201, 'HNKU1009739', 20, 21756, 2140, 'ВНТР', 0, 0, 'O-T-P-R', '']]]}
+
+
+"""Функция разделения таблицы платформ на уникальные платформы(типы) и количество таких типов в таблице
 Структура:
-{sessionID_1: [[plats], [conts]], sessionID_2: [[plats], [conts]], ...}"""
+[plat_1, plat_2, ...], {(plat_1): count_1, (plat_2): count_2, ...}"""
 
 
 def generate_P_and_duplP(platforms_table):
     plats, count_platforms = np.unique(platforms_table, axis=0, return_counts=True)
     dupl_platforms = dict(zip((tuple(np.array(row, dtype=object)) for row in plats), map(int, count_platforms)))
     return plats.tolist(), dupl_platforms
+
+
+"""Функция разделения входных таблиц контейнеров и платформ на сессии и разделение таблицы платформ на типы и количество
+таких типов
+Структура:
+{sessionID_1: [[[plat_1_1, plat_1_2, ...], {(plat_1_1): count_1_1, (plat_1_2): count_1_2, ...}], [conts]], 
+sessionID_2: [[[plat_2_1, plat_2_2, ...], {(plat_2_1): count_2_1, (plat_2_2): count_2_2, ...}], [conts]], ...}"""
 
 
 def P_and_C_test(platforms_table, cont_table):
@@ -146,26 +172,26 @@ def P_and_C_test(platforms_table, cont_table):
     result = {}
     for session in sessions:
         if session not in result.keys():
-            result[session] = [[[], []], []]
+            result[session] = [[[], {}], []]
         result[session][0][0].extend((platforms_table[platforms_table[:, 0] == session]).tolist())
         result[session][0][0], result[session][0][1] = generate_P_and_duplP(result[session][0][0])
         result[session][1].extend((cont_table[cont_table[:, 0] == session]).tolist())
 
+        print(session)
+        result[session][1] = generate_combinations_and_permutations_of_cont(result[session][1])
+        # print(result[session][1])
+    # return None
     return result
 
 
-print(P_and_C_test(platforms_array, containers_array))
+# SC = P_and_C_test(platforms_array, containers_array)
+# with open('SC.yaml', 'w', encoding='utf-8') as file:
+#     yaml.dump(SC, file, allow_unicode=True)
+# print(SC[17])
 
-#   Создаём сокращенное множество типов платформ P(т.е. уникальные строки без учёта SessionID и carNumber)
-# def create_P_and_C(sessionID_platf, sessionID_cont):
-# P, count_P = np.unique(P, axis=0, return_counts=True)
-# dupl_P = dict(zip((tuple(map(str, row)) for row in P), map(int, count_P)))
-#   P - множество типов платформ
-#   dupl_P - словарь {тип платформы: количество таких платформ}
 
 #   Создаём множество всех контейнеров C
-C = np.delete(np.array(containers_array), 8, 1)
-C = np.unique(C, axis=0)
+C = np.array(containers_array)
 
 #   Создаём множество контейнеров, сгруппированных по приоритету отправки
 O = np.unique(C[:, 5])  # мн-во приоритетов контейнеров
@@ -193,26 +219,24 @@ for platform in platforms_rules_1:
     cont_rules_1.append((np.array(containers_rules_array)[np.array(containers_rules_array)[:, 1] == platform]).tolist())
 
 containersSPR = sort_SPR(cont_rules_1)
-# print(containersSPR)
+with open('containersSPR.json', 'w', encoding='utf-8') as file:
+    json.dump(containersSPR, file, ensure_ascii=False, indent=4)
 
 
-# """Работаем с таблицей RulesSPR"""
-# rules = np.array(containers_on_platforms_rules_array)
-# codes = np.unique(rules[:, 0])
-# rules_2 = []
-# for code in codes:
-#     rules_2.append(rules[rules[:, 0] == code].tolist())
-#
-# rulesSPR = sort_SPR(rules_2)
-#
-# """Работаем с таблицей TablesSPR"""
-# tables = np.array(containers_on_platforms_tables_array)
-# codes = np.unique(tables[:, 0])
-# tables_new = []
-# for code in codes:
-#     tables_new.append(tables[tables[:, 0] == code].tolist())
-#
-# tablesSPR = sort_SPR(tables_new)
+"""Работаем с таблицей RulesSPR"""
+rules = np.array(containers_on_platforms_rules_array)
+codes = np.unique(rules[:, 0])
+rules_2 = []
+for code in codes:
+    rules_2.append(rules[rules[:, 0] == code].tolist())
+
+
+"""Работаем с таблицей TablesSPR"""
+tables = np.array(containers_on_platforms_tables_array)
+codes = np.unique(tables[:, 0])
+tables_new = []
+for code in codes:
+    tables_new.append(tables[tables[:, 0] == code].tolist())
 
 
 cursor.close()
